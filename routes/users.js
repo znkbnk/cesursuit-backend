0f1fc709f8315f04a830c09f5b84e5b975329ae2
Jevin <zenikibeniki@gmail.com>
2025-05-04 17:24:48 +0100

    users.js serviceKey

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,11 @@ const serviceAccount = require("./serviceAccountKey.json");
 // Initialize Firebase Admin if not already
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
