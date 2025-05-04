4e5211d170e6b7789b525b3b01179e163c1c6579
Jevin <zenikibeniki@gmail.com>
2025-05-04 17:26:55 +0100

    serviceKEy 2

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const admin = require("firebase-admin");
 const router = express.Router();
-const serviceAccount = require("./serviceAccountKey.json");
 
 // Initialize Firebase Admin if not already
 if (!admin.apps.length) {
